3b84b3c78a96375117e24b10d9b0f2cbd8faf363
joselyn-perez-granados-1088 <168213224+joselyn-perez-granados-1088@users.noreply.github.com>
2025-02-07 16:47:50 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,20 @@
+//funcion que no devuelva valor
+//funcion saludar
+void saludar(){
+  print('hola mundo');
+}
+
+void escribenombre(){
+print('Joselyn Perez Granados')
+}
+
+void main(){
+  //Joselyn Perez Granados mat: 22308051281088
+  //llamar a la funcion saludar
+  print('Joselyn Perez Granados mat: 22308051281088');
+  print('Llamando a la funcion saludar');
+  saludar();
+  print('Llamando a la funcion escribenombre');
+  escribenombre();
+  
+}
